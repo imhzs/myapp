b8c10c9b02f5c6d1ad0cadadd45dc7ee1a59299a
imhzs <30996509+imhzs@users.noreply.github.com>
2018-04-02 09:29:18 +0800

    Update noop-interceptor.ts

--- src/app/http-interceptors/noop-interceptor.ts
+++ src/app/http-interceptors/noop-interceptor.ts
@@ -5,17 +5,21 @@ import {
 
 import { Observable } from 'rxjs/Observable';
 
+import { Storage } from '@ionic/storage';
+
 /** Pass untouched request through to the next request handler. */
 @Injectable()
 export class NoopInterceptor implements HttpInterceptor {
-
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Authorization');
+    let dupReq: HttpRequest = req;
     if (!req.headers.has('Authorization')) {
       let token = localStorage.getItem('token');
-      req.headers.set('Authorization', `Bearer ${token}`);
+      dupReq = req.clone({
+            headers: req.headers.set('Authorization', `Bearer ${token}`)
+          });
     }
-    let resp = next.handle(req);
+    let resp = next.handle(dupReq);
     console.log(resp.take(1));
     return resp;
   }
