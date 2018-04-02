8226df62814f7e9149369159227c7f0e4498ed4f
imhzs <30996509+imhzs@users.noreply.github.com>
2018-04-02 09:22:27 +0800

    Update index.ts

--- src/app/http-interceptors/index.ts
+++ src/app/http-interceptors/index.ts
@@ -1,9 +1,17 @@
 /* "Barrel" of Http Interceptors */
+import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { NoopInterceptor } from './noop-interceptor';
 
 /** Http interceptor providers in outside-in order */
-export const httpInterceptorProviders = [
+/*export const httpInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: NoopInterceptor, multi: true },
-];
+];*/
+
+@NgModule({
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: NoopInterceptor, multi: true }
+  ]
+})
+export class InterceptorModule { }
